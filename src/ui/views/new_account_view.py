89a1a199341369c6273f34e17297461c78a28259
Riku Rauhala <riku@rauhala.io>
2022-04-08 18:59:17 +0300

    Refactor widget initialization

--- src/ui/views/new_account_view.py
+++ src/ui/views/new_account_view.py
@@ -59,36 +59,53 @@ class NewAccountView:
             self._ent_name.delete(0, END)
             self._ent_type.delete(0, END)
 
-    def _initialize(self):
-        self._frame = ttk.Frame(master=self._root)
-
+    def _initialize_title_label(self):
         txt_title = "Add new account"
         lbl_title = ttk.Label(master=self._frame, text=txt_title)
         lbl_title.grid(row=0, column=0, columnspan=2,
                        padx=self._padx, pady=self._pady, sticky=constants.W)
 
+    def _initialize_name_label(self):
         txt_name = "Name"
         lbl_name = ttk.Label(master=self._frame, text=txt_name)
-        lbl_name.grid(row=1, column=0, padx=self._padx, pady=self._pady)
+        lbl_name.grid(row=1, column=0, padx=self._padx,
+                      pady=self._pady, sticky=constants.E)
 
+    def _initialize_name_entry(self):
         self._ent_name = ttk.Entry(master=self._frame)
         self._ent_name.grid(row=1, column=1, padx=self._padx, pady=self._pady)
 
+    def _initialize_type_label(self):
         txt_type = "Type"
         lbl_type = ttk.Label(master=self._frame, text=txt_type)
-        lbl_type.grid(row=2, column=0, padx=self._padx, pady=self._pady)
+        lbl_type.grid(row=2, column=0, padx=self._padx,
+                      pady=self._pady, sticky=constants.E)
 
+    def _initialize_type_entry(self):
         self._ent_type = ttk.Entry(master=self._frame)
         self._ent_type.grid(row=2, column=1, padx=self._padx, pady=self._pady)
 
+    def _initialize_cancel_button(self):
         txt_cancel = "Cancel"
         btn_cancel = ttk.Button(master=self._frame, text=txt_cancel,
                                 command=self._handle_cancel)
         btn_cancel.grid(row=3, column=0, padx=self._padx,
                         pady=self._pady, sticky=constants.EW)
 
+    def _initialize_submit_button(self):
         txt_submit = "Submit"
         btn_submit = ttk.Button(master=self._frame, text=txt_submit,
                                 command=self._handle_submit)
         btn_submit.grid(row=3, column=1, padx=self._padx,
                         pady=self._pady, sticky=constants.EW)
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+
+        self._initialize_title_label()
+        self._initialize_name_label()
+        self._initialize_name_entry()
+        self._initialize_type_label()
+        self._initialize_type_entry()
+        self._initialize_cancel_button()
+        self._initialize_submit_button()
