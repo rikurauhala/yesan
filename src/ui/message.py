65bd1e9591fb7a0985596a6890f662283cdb59bf
Riku Rauhala <riku@rauhala.io>
2022-04-30 11:12:24 +0300

    Refactor message.py

--- src/ui/message.py
+++ src/ui/message.py
@@ -10,26 +10,32 @@ class Message:
         self._var_message = var_message
         self._lbl_message = None
 
-    def get_message(self, mode):
+    def _get_error_message(self, mode):
         if mode == "missing":
             self._var_message.set("Importing accounts is not supported yet!")
-            self._lbl_message = ttk.Label(
-                master=self._frame,
-                textvariable=self._var_message,
-                foreground=colors.ERROR
-            )
-        if mode == "error":
+        elif mode == "error":
             self._var_message.set("Exporting accounts failed!")
-            self._lbl_message = ttk.Label(
-                master=self._frame,
-                textvariable=self._var_message,
-                foreground=colors.ERROR
-            )
+
+        self._lbl_message = ttk.Label(
+            master=self._frame,
+            textvariable=self._var_message,
+            foreground=colors.ERROR
+        )
+
+    def _get_success_message(self, mode):
         if mode == "success":
             self._var_message.set("Accounts exported successfully!")
-            self._lbl_message = ttk.Label(
-                master=self._frame,
-                textvariable=self._var_message,
-                foreground=colors.SUCCESS
-            )
+        
+        self._lbl_message = ttk.Label(
+            master=self._frame,
+            textvariable=self._var_message,
+            foreground=colors.SUCCESS
+        )
+
+    def get_message(self, mode):
+        if mode == "missing" or mode == "error":
+            self._get_error_message(mode)
+        else:
+            self._get_success_message(mode)
+        
         return self._lbl_message
