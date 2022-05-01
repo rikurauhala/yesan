00c34e3b987aaa1839156def5ffbc6a40feeb75a
Riku Rauhala <riku@rauhala.io>
2022-05-01 22:16:27 +0300

    Remove docstring from tasks.py

--- tasks.py
+++ tasks.py
@@ -3,42 +3,35 @@ from invoke import task
 
 @task
 def init(ctx):
-    """Initialize the database."""
     ctx.run("python3 src/database.py")
 
 
 @task
 def start(ctx):
-    """Run the application."""
     ctx.run("python3 src/index.py")
 
 
 @task
 def test(ctx):
-    """Run automated tests."""
     ctx.run("pytest src", pty=True)
 
 
 @task
 def coverage(ctx):
-    """Check the coverage of the automated tests."""
     ctx.run("coverage run --branch -m pytest src", pty=True)
 
 
 @task(coverage)
 def coverage_report(ctx):
-    """Generate a test coverage report."""
     ctx.run("coverage html")
 
 
 @task
 def lint(ctx):
-    """Check the quality of source code."""
     ctx.run("pylint src")
 
 
 @task
 def format(ctx):
-    """Format the code according to the PEP8 style guide."""
     ctx.run("autopep8 --in-place --recursive src")
 
