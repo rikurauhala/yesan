867420e839d607652df96c183779ac271853b9c7
Riku Rauhala <riku@rauhala.io>
2022-05-03 21:07:28 +0300

    Add docstring to transaction.py

--- src/entities/account.py
+++ src/entities/account.py
@@ -2,7 +2,7 @@ class Account:
     """Account object, stores properties of an account.
 
     Attributes:
-        id: UUID, unique identifier of an account.
+        id: String, unique identifier of an account in the UUID format.
         name: String, name of an account.
         type: String, type of an account.
     """
@@ -11,7 +11,7 @@ class Account:
         """Initializes a new instance of an Account object.
 
         Args:
-            account_id (UUID): Unique identifier of an account.
+            account_id (String): Unique identifier of an account in the UUID format.
             account_name (String): Name of an account.
             account_type (String): Type of an account.
         """
--- src/entities/transaction.py
+++ src/entities/transaction.py
@@ -1,6 +1,29 @@
 class Transaction:
+    """Transaction object, stores properties of a transaction.
+
+    Attributes:
+        id: String, unique identifier of a transaction in the UUID format.
+        date: Timestamp, date when the transaction was made. 
+        amount: Integer, amount of money involved in the transaction.
+        category: String, category to which the transaction belongs to.
+        description: String, describes the purpose of the transaction.
+        account_id: String, connects the transaction to an account.
+        party: String, specifies the other party (payer/receiver) of the transaction.
+    """
 
     def __init__(self, transaction_id, date, amount, category, description, account_id, party):
+        """Initializes a new instance of a Transaction object.
+
+        Args:
+            transaction_id (String): Unique identifier of a transaction in the UUID format.
+            date (Timestamp): Date when the transaction was made. 
+            amount (Integer): Amount of money involved in the transaction.
+            category (String): String, category to which the transaction belongs to.
+            description (String): Describes the purpose of the transaction.
+            account_id (String): Connects the transaction to an account.
+            party (String): Specifies the other party (payer/receiver) of the transaction.
+        """
+
         self.id = transaction_id
         self.date = date
         self.amount = amount
