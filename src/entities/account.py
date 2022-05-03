847a6ff857eb129b67f0acb9adcbcb9ca06dea12
Riku Rauhala <riku@rauhala.io>
2022-05-03 20:18:28 +0300

    Add docstring to account.py

--- src/entities/account.py
+++ src/entities/account.py
@@ -1,6 +1,21 @@
 class Account:
+    """Account object, stores properties of an account.
+
+    Attributes:
+        id: UUID, unique identifier of an account.
+        name: String, name of an account.
+        type: String, type of an account.
+    """
 
     def __init__(self, account_id, account_name, account_type):
+        """Initializes a new instance of an Account object.
+
+        Args:
+            account_id (UUID): Unique identifier of an account.
+            account_name (String): Name of an account.
+            account_type (String): Type of an account.
+        """
+
         self.id = account_id
         self.name = account_name
         self.type = account_type
