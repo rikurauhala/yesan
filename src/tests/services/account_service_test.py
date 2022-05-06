b72bf03970237464b0476883b8d479658e43d299
Riku Rauhala <riku@rauhala.io>
2022-05-06 23:36:44 +0300

    Add more tests for account service

--- src/tests/services/account_service_test.py
+++ src/tests/services/account_service_test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from file_handler import file_handler
+
 from services.account_service import AccountService
 
 
@@ -8,46 +10,68 @@ class TestAccountService(unittest.TestCase):
         self._account_service = AccountService()
         self._account_service.delete_all()
 
-        self._name_account_a = "Checking account"
-        self._type_account_a = "Bank account"
+        self._account_a_name = "Name A"
+        self._account_a_type = "Type A"
 
-        self._name_account_b = "Bitcoin"
-        self._type_account_b = "Crypto currency"
+        self._account_b_name = "Name B"
+        self._account_b_type = "Type"
+
+        self._file_handler = file_handler
 
     def test_create_account(self):
         self._account_service.create_account(
-            self._name_account_b,
-            self._type_account_b
+            self._account_b_name,
+            self._account_b_type
         )
         accounts = self._account_service.find_all()
 
         self.assertEqual(len(accounts), 1)
-        self.assertEqual(accounts[0].name, self._name_account_b)
-        self.assertEqual(accounts[0].type, self._type_account_b)
+        self.assertEqual(accounts[0].name, self._account_b_name)
+        self.assertEqual(accounts[0].type, self._account_b_type)
 
     def test_get_list(self):
         self._account_service.create_account(
-            self._name_account_a,
-            self._type_account_a
+            self._account_a_name,
+            self._account_a_type
         )
         self._account_service.create_account(
-            self._name_account_b,
-            self._type_account_b
+            self._account_b_name,
+            self._account_b_type
         )
 
         account_list = self._account_service.get_list()
 
-        self.assertEqual(account_list[1], self._name_account_a)
-        self.assertEqual(account_list[0], self._name_account_b)
+        self.assertEqual(account_list[0], self._account_a_name)
+        self.assertEqual(account_list[1], self._account_b_name)
 
     def test_get_id_by_name(self):
         self._account_service.create_account(
-            self._name_account_b,
-            self._type_account_b
+            self._account_b_name,
+            self._account_b_type
         )
         accounts = self._account_service.find_all()
         id_expected = accounts[0].id
-        id_actual = self._account_service.get_id_by_name(self._name_account_b)
+        id_actual = self._account_service.get_id_by_name(self._account_b_name)
 
         self.assertEqual(len(accounts), 1)
         self.assertEqual(id_actual, id_expected)
+
+    def test_import_accounts(self):
+        success = self._account_service.create_account(
+            self._account_b_name,
+            self._account_b_type
+        )
+        self.assertTrue(success)
+        success = self._account_service.export_accounts()
+        self.assertTrue(success)
+        accounts = self._file_handler.import_accounts()
+        self.assertEqual(len(accounts), 1)
+
+    def test_export_accounts(self):
+        success = self._account_service.create_account(
+            self._account_b_name,
+            self._account_b_type
+        )
+        self.assertTrue(success)
+        success = self._account_service.export_accounts()
+        self.assertTrue(success)
