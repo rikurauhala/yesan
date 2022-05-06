f363a0aa74ab66bed67ef0e27821a703ce3add0f
Riku Rauhala <riku@rauhala.io>
2022-05-06 14:40:49 +0300

    Refactor transaction service test

--- src/tests/services/transaction_service_test.py
+++ src/tests/services/transaction_service_test.py
@@ -1,27 +1,43 @@
 import unittest
 
+from services.account_service import AccountService
 from services.transaction_service import TransactionService
 
 
 class TestTransactionService(unittest.TestCase):
     def setUp(self):
+        self._account_service = AccountService()
+        self._account_service.delete_all()
+
+        self._account_a_name = "Name A"
+        self._account_a_type = "Type A"
+
+        self._account_service.create_account(
+            self._account_a_name,
+            self._account_a_type
+        )
+
+        self._account_a_uuid = self._account_service.find_all()[0].id
+
         self._transaction_service = TransactionService()
         self._transaction_service.delete_all()
 
-        self._date_transaction_a = "2022-01-01"
-        self._amount_transaction_a = "-9999"
-        self._category_transaction_a = "Category A"
-        self._description_transaction_a = "Description A"
-        self._account_id_transaction_a = "Account_id A"
-        self._party_transaction_a = "Receiver A"
+        self._transaction_a_date = "2022-01-01"
+        self._transaction_a_amount = "-9999"
+        self._transaction_a_category = "Category A"
+        self._transaction_a_description = "Description A"
+        self._transaction_a_account_id = self._account_a_uuid
+        self._transaction_a_party = "Receiver A"
 
     def test_create_transaction(self):
-        self._transaction_service.create_transaction(
-            self._date_transaction_a,
-            self._amount_transaction_a,
-            self._category_transaction_a,
-            self._description_transaction_a,
-            self._account_id_transaction_a,
-            self._party_transaction_a
+        success = self._transaction_service.create_transaction(
+            self._transaction_a_date,
+            self._transaction_a_amount,
+            self._transaction_a_category,
+            self._transaction_a_description,
+            self._transaction_a_account_id,
+            self._transaction_a_party
         )
         transactions = self._transaction_service.find_all()
+        self.assertTrue(success)
+        self.assertEqual(len(transactions), 1)
