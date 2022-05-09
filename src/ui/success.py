8cc691b7bee7a12fe57c898d538b63d181553cb0
Riku Rauhala <riku@rauhala.io>
2022-05-09 09:02:26 +0300

    Refactor message system

---
+++ src/ui/success.py
@@ -0,0 +1,9 @@
+success = {
+    "s-01": "Accounts exported successfully!",
+    "s-02": "New account added!",
+    "s-03": "Transactions exported successfully!",
+    "s-04": "New transaction added!",
+    "s-05": "Accounts imported successfully!",
+    "s-06": "Transactions imported successfully!",
+    "s-07": "Settings saved successfully!",
+}
